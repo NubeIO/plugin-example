5fcf6a94349d12928540555671d846b301d748b7
NubeDev <aidan@enviroservices.com.au>
2024-07-10 09:37:18 +1000

    setup of repo

---
+++ maths/test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/NubeIO/plugin-example/maths/add"
+	"github.com/NubeIO/plugin-example/maths/subtract"
+	"github.com/NubeIO/rxlib/libs/pluginlib"
+)
+
+var name = "math-plugin"
+
+func main() {
+
+	factory := pluginlib.New(name)
+	factory.AddPallet("add", add.New)
+	factory.AddPallet("subtract", subtract.New)
+	factory.Register(name)
+}
